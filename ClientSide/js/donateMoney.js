4642b239a537e1ce2c12f0fa9d9a343736b84c41
avally0611 <avally7860@gmail.com>
2024-09-24 17:17:46 +0200

    made a basic payment screen

--- ClientSide/js/donateMoney.js
+++ ClientSide/js/donateMoney.js
@@ -1,6 +1,11 @@
+const donateButton = document.getElementById('donate-button');
+donateButton.addEventListener('click', donateButtonClicked);
+
 window.addEventListener('load', function () {
     console.log('All assets are loaded');
     fetchCharitiesforDropdown();
+
+    
 });
 
 function fetchCharitiesforDropdown() {
@@ -76,4 +81,8 @@ function searchComboBox(input, charities){
     });
 }
 
+function donateButtonClicked()
+{
+    window.location.href = "payment.html";
+}
 
