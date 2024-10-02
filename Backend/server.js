63d7b8bd25610d9acb507ec4269b9b0d0a8f2424
avally0611 <avally7860@gmail.com>
2024-10-02 13:16:50 +0200

    ADD DONATION ALGORITHM FINISHED AND WORKING

--- Backend/server.js
+++ Backend/server.js
@@ -52,7 +52,7 @@ app.post("/api/verifyLogin", function (req, res)
 
     console.log(username, password);
 
-    connection.query(`SELECT password FROM pat_2024.donors WHERE username = '${username}'`, function(err, result) 
+    connection.query('SELECT password FROM pat_2024.donors WHERE username = ?', [username], function(err, result) 
     {
       if (err) 
       {
@@ -95,7 +95,7 @@ app.post("/api/addUser", function (req, res)
     console.log(username, password, fname, lname, email, pnum);
 
     //perform a insert query
-    connection.query(`INSERT INTO pat_2024.donors (first_name, last_name, email, phone_number, username, password) VALUES ("'${fname}'", "'${lname}'", "'${email}'", "'${pnum}'", "'${username}'", "'${password}'");`, function(err, result) 
+    connection.query('INSERT INTO pat_2024.donors (first_name, last_name, email, phone_number, username, password) VALUES (?, ?, ?, ?, ?, ?)', [fname, lname, email, pnum, username, password], function(err, result) 
     {
       if (err) 
       {
@@ -138,7 +138,8 @@ app.get("/api/requests", function (req, res)
   //this will hold JSON array with charity objects which have a name and an array of requests (item name and quantity needed)
     var requests = [];
 
-    connection.query(`SELECT DISTINCT name FROM pat_2024.requests INNER JOIN charities ON requests.charity_id = charities.charity_id`, function(err, resultCharities) 
+    //get the list of charities which have a request
+    connection.query(`SELECT DISTINCT name FROM pat_2024.requests INNER JOIN charities ON requests.charity_id = charities.charity_id WHERE status = 0`, function(err, resultCharities) 
     {
       if (err) 
       {
@@ -157,7 +158,7 @@ app.get("/api/requests", function (req, res)
 
       resultCharities.forEach(charity => {
 
-        connection.query(`SELECT request_id, item_name, quantity FROM pat_2024.requests INNER JOIN charities ON requests.charity_id = charities.charity_id  INNER JOIN items ON items.item_id = requests.item_id WHERE name = '${charity.name}'`, function(err, result)
+        connection.query(`SELECT request_id, item_name, quantity FROM pat_2024.requests INNER JOIN charities ON requests.charity_id = charities.charity_id  INNER JOIN items ON items.item_id = requests.item_id WHERE name = ? AND status = 0`, [charity.name], function(err, result)
         {
           if (err) 
           {
@@ -187,17 +188,20 @@ app.get("/api/requests", function (req, res)
 
 });
 
-
+//this is a post request to add a donation to the database
 app.post("/api/addDonation", function (req, res)
 {
-  //we passed an array 
+  //gets all fields from the body
     const username = req.body.username;
-    const donor_id = 0;
     const cartItem = req.body.cartItem;
     const request_id = cartItem.request_id;
     const donation_quantity = cartItem.donation_quantity;
 
-    connection.query(`SELECT donor_id FROM pat_2024.donors WHERE username = '${username}'`, function(err, result) 
+
+    console.log(username);
+
+    //gets the donor id using username (we can assume username is unique as their is a mrthod that makes sure ecah donor has unique username)
+    connection.query('SELECT donor_id FROM pat_2024.donors WHERE username = ?', [username], function(err, result) 
     {
       if (err) 
       {
@@ -211,50 +215,82 @@ app.post("/api/addDonation", function (req, res)
         return;
       }
       console.log(result);
-      donor_id = result[0].donor_id;
+      const donor_id = parseInt(result[0].donor_id);
 
-    });
-
-    console.log(donor_id)
-
-    connection.query(`INSERT INTO pat_2024.donations (donor_id) VALUES ("'${donor_id}'");`, function(err, result) 
-    {
-      if (err) 
+      //then inserts into donations table using donor id that we got from the previous query
+      connection.query('INSERT INTO pat_2024.donations (donor_id) VALUES (?)', [donor_id], function(err, result) 
       {
-        console.error('error inserting into donations:', err);
-        res.status(500).send('error inserting into donations');
-        return;
-      }
-      
-      console.log('true');
-      res.send('true');
+        if (err) 
+        {
+          console.error('error inserting into donations:', err);
+          res.status(500).send('error inserting into donations');
+          return;
+        }
+        
+        console.log('true');
+        //when you do an insert query, you can use insertId to get the id of the row that was inserted (since it is a primary key)
+        const donation_id = parseInt(result.insertId);
+
+        //insert into donations_entry using donation id that we got and body elements
+        connection.query('INSERT INTO pat_2024.donations_entry (donation_id, request_id, quantity) VALUES (?, ?, ?)', [donation_id, request_id, donation_quantity], function(err, result) 
+        {
+          if (err) 
+          {
+            console.error('Error inserting into donations_entry :', err);
+            res.status(500).send('eror inserting into donations_entry');
+            return;
+          }
+          
+          console.log('true');
 
 
+          //NOW WE HAVE TO UPDATE REQUESTS DB
+
+          //get the quantity needed of the request from the body
+          const quantityNeeded = parseInt(cartItem.donation_quantity_needed);
+
+          if (donation_quantity == quantityNeeded)
+          {
+            //update status of request to 0 to show that it is fulfilled
+            connection.query('UPDATE pat_2024.requests SET status = 1 WHERE request_id = ?', [request_id], function(err, result) 
+            {
+              if (err) 
+              {
+                console.error('Error updating request status:', err);
+                res.status(500).send('error updating request status');
+                return;
+              }
+              
+              console.log('true');
+              res.send('true');
+            });
+          } 
+          else if (donation_quantity < quantityNeeded) 
+          {
+            // update the quantity needed in the requests table
+            const updatedQuantity = quantityNeeded - donation_quantity;
+            connection.query('UPDATE pat_2024.requests SET quantity = ? WHERE request_id = ?', [updatedQuantity, request_id], function(err, result) 
+            {
+              if (err) 
+              {
+                console.error('Error updating request quantity:', err);
+                res.status(500).send('error updating request quantity');
+                return;
+              }
+              
+              console.log('true');
+              res.send('true');
+            });
+          }
+          
+
+        });
+
+
+      });
 
     });
 
-    const donation_id = result.insertId;
-
-
-    //insert into donations_entry
-    connection.query(`INSERT INTO pat_2024.donations_entry (donor_id, request_id, quantity) VALUES ("'${donor_id}'", "'${request_id}'", "'${donation_quantity}'");`, function(err, result) 
-    {
-      if (err) 
-      {
-        console.error('Error inserting into donations_entry :', err);
-        res.status(500).send('eror inserting into donations_entry');
-        return;
-      }
-      
-      console.log('true');
-      res.send('true');
-
-
-
-    });
-
-
-
-
+    
 
 });
